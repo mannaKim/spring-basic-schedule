267ba4b2e41320aadf6bad063c3a3ee707bf07b8
hanna <hannah.k0925@gmail.com>
2025-01-24 11:03:20 +0900

    chore: Create basic structure

---
+++ src/main/java/com/example/schedule/SpringBasicScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicScheduleApplication.class, args);
+	}
+
+}
